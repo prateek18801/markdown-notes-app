1da58e79fe15da02bcc22ef7f37f2b30b9b07c40
Prateek Chaurasia <prateek18801@gmail.com>
2023-03-24 11:08:42 +0530

    Setup routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+
 const App = () => {
     return (
-        <>
-            <p>Hello World</p>
-        </>
+        <Routes>
+            <Route path='/' element={<h1>Notes</h1>} />
+            <Route path='/note' element={<h1>New Note</h1>} />
+            <Route path='/:id'>
+                <Route index element={<h1>note id_</h1>} />
+                <Route path='edit' element={<h1>Edit note id_</h1>} />
+                <Route path='delete' element={<h1>Delete note id_</h1>} />
+            </Route>
+            <Route path='*' element={<h1>Page Not Found</h1>} />
+        </Routes>
     );
 }
 
