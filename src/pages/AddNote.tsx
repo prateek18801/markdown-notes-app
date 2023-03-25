608c7908b2c1fbe08994d38c659c68449e7c77c7
Prateek Chaurasia <prateek18801@gmail.com>
2023-03-25 23:41:40 +0530

    Pass props to NoteForm

--- src/pages/AddNote.tsx
+++ src/pages/AddNote.tsx
@@ -1,11 +1,12 @@
 import { Container } from 'react-bootstrap';
 import NoteForm from '../components/NoteForm';
+import { AddNoteProps } from '..';
 
-const AddNote = () => {
+const AddNote = ({ onSubmit, onAddTag, availableTags }: AddNoteProps) => {
     return (
         <Container>
             <h1>Add Note</h1>
-            <NoteForm />
+            <NoteForm onSubmit={onSubmit} onAddTag={onAddTag} availableTags={availableTags} />
         </Container>
     );
 }
